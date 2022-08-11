9c9222f5a6706a845aba5091e5a9465d2337f8ca
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-11 10:34:48 +0530

    Added:Win or loose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,15 +1,26 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
-static int STAKE=100;
-static int BET=1;
+	public static final int PLAYER_BALANCE=100;
+	public static final int BET_PER_GAME=1;
+	public static final int WON_BET=1;
+	public static final int LOST_BET=0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Welcome to Gambling Simulator");
-		System.out.println("Stake amount:"+STAKE);
-		System.out.println("Betting amount for each game:"+BET);
-		
-	}
+		int currentBalance=PLAYER_BALANCE;
 
+		int choice=(int)(Math.random()*10%2);
+
+		switch(choice) {
+		case WON_BET:
+			currentBalance++;
+			break;
+
+		case LOST_BET:
+			currentBalance--;
+			break;
+		}
+		System.out.println("Stake amount:"+currentBalance);
+	}
 }
