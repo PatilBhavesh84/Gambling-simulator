9a85801d228ce72463838ed1a39513cdcdca6764
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-11 10:14:13 +0530

    Added:Gambler starts with 100 stake

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,10 +1,15 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
+static int STAKE=100;
+static int BET=1;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Gambling Simulator");
+		System.out.println("Stake amount:"+STAKE);
+		System.out.println("Betting amount for each game:"+BET);
+		
 	}
 
 }
